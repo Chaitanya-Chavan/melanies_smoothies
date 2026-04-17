6e659789a767909512e9934b494d42abd61ab366
Chaitanya Chavan <147969147+Chaitanya-Chavan@users.noreply.github.com>
2026-04-17 17:37:11 +0530

    Update streamlit_app.py
    Added the packages

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,7 @@ if ingredients_list:
         #st.success('Your Smoothie is ordered!', icon="✅" +name_on_order!)
         st.success("Your Smoothie is ordered, " + name_on_order + "!", icon="✅")
 
-
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
